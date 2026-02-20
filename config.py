eb6d46f4d2936d055855d7dc0772348fdc7e7307
Harshini Chanduluri <harshinichanduluri@gmail.com>
2026-02-20 08:16:52 +0530

    Initial commit - SmartCart SQLite project

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+# ------------------------------------------
+# Stores all configuration settings
+# ------------------------------------------
+
+SECRET_KEY = "your_secret_key"
+
+
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'harshinichanduluri@gmail.com'
+MAIL_PASSWORD = 'pczb hxee wydf djnn'
+MAIL_DEFAULT_SENDER = 'harshinichanduluri@gmail.com'# Gmail App Password
+
+#Razor pay
+RAZORPAY_KEY_ID = "rzp_test_SFy2r3bzy9Ug2M"
+RAZORPAY_KEY_SECRET = "TZBrjr53yqnfAih3xRq7R7cD"
+
---
+++ init_db.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+
+DB_NAME = "smartcart.db"   # Database name
+
+def init_db():
+    # Connect to SQLite database
+    conn = sqlite3.connect(DB_NAME)
+    
+    # Enable foreign key support
+    conn.execute("PRAGMA foreign_keys = ON")
+    
+    # Execute schema file
+    with open("schema.sql", "r", encoding="utf-8") as f:
+        conn.executescript(f.read())
+    
+    conn.commit()
+    conn.close()
+    
+    print("SmartCart database initialized successfully.")
+
+if __name__ == "__main__":
+    # Optional: remove old database during development
+    if os.path.exists(DB_NAME):
+        os.remove(DB_NAME)
+        print("Old smartcart database removed.")
+
+    init_db()
